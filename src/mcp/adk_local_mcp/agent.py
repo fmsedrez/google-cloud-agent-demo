9f2db9f774b09e75c79d6f528ca12e02ad8c9352
Fernando Sedrez <hello@fmsedrez.dev>
2025-07-23 08:55:07 -0300

    feat: Refactors build process with Makefile and updates ADK path
    Updates the build process by introducing a Makefile for simplified task execution and management.
    
    Refactors the ADK server path within the MCP agent to enhance flexibility and robustness, ensuring the server is run from the correct directory.

--- src/mcp/adk_local_mcp/agent.py
+++ src/mcp/adk_local_mcp/agent.py
@@ -1,10 +1,11 @@
+import os
+
 from google.adk.agents import LlmAgent
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioServerParameters
 
 from .prompts import root_agent_instructions
 
 MODEL = "gemini-2.5-flash"
-MCP_SERVER_SCRIPT = "src/mcp/adk_mcp_server.py"
 
 root_agent = LlmAgent(
     model=MODEL,
@@ -14,8 +15,11 @@ root_agent = LlmAgent(
     tools=[
         MCPToolset(
             connection_params=StdioServerParameters(
-                command='python3',
-                args=[MCP_SERVER_SCRIPT],
+                command='uv',
+                args=["--directory",
+                      os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"),
+                      "run",
+                      "adk_mcp_server.py"],
             )
         )
     ],
